fab9fa66cd82f8db977dedd90e28e60468124183
dnnsys <as@rsdigital.ru>
2017-10-20 17:51:14 +0300

    updated landing

--- resources/assets/javascripts/modules/TestimonialsSlider.js
+++ resources/assets/javascripts/modules/TestimonialsSlider.js
@@ -12,17 +12,21 @@ export default class TestimonialsSlider{
         $('#testimonialContainer').owlCarousel({
             margin: 40,
             loop: true,
+            nav: false,
+            navText: ['<i class="fa fa-arrow-left" aria-hidden="true"></i>','<i class="fa fa-arrow-right" aria-hidden="true"></i>'],
             responsive: {
                 992: {
                     items: 3,
+                    nav: true,
                 },
                 640: {
                     items: 2,
+                    nav: true,
                 },
                 480: {
                     margin: 10,
                     stagePadding: 20,
-                    items: 1
+                    items: 1,
                 },
                 0: {
                     margin: 10,
